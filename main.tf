4950148546ee8478e08666fcb03e7ea7d4ba7932
Ahmad Saeed <Ahmad.Saeed@Cdocs.local>
2024-06-25 18:15:14 +0500

    tflint and .yml fixed

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.55.0"  # Use the version you're currently using
+    }
+  }
+}
+
 module "s3_bucket" {
   source = "./modules/s3"
 
